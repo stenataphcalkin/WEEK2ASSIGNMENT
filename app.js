8288317d411f68a99c43675aaf69702d8ca3c1d5
Steph <steph@polyhydral.co.uk>
2025-08-22 11:36:22 +0100

    updated readme

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ const images1exampleonly = [
 ];
 
 //TODO: I need to create thumbnail images
-// FOR EACH COMMENT REPLACE IT WITH ACTUAL CODE -->
-// 
+//REPLACE WITH ACTUAL CODE -->
+
+// Function > createThumbnails
+// Select the DOM element (thumbnail-container) to contain our thumbnails 
+//THIS IS A REPETITIVE TASK --> Loops through our array using the length property
+//loop inside
+// - create img element
+// - update src and alt attributes of the img element to match those in the array(PARAMETERS - Manny hint)
+// - give each img a className (need to make them small. img.className etc)
+// - add an event listener to each image --> the event handle of this listener is the function you write to create large images (MANNY HINT)
+// - append the created images to the thumbnail-container
 
 //TODO: I need to create my large images
