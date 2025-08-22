38c60a25b835f8da0f37d676ac3c3ad1519bb325
Steph <steph@polyhydral.co.uk>
2025-08-22 11:26:29 +0100

    feat: store image data in array of objects

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("Kupo!");
+
+//TODO: I need to store my image data
+//Can use images stored locally or remotely
+const images = ["url", "url", "url"];
+const alts = ["alt", "alt", "alt"];
+
+const images1exampleonly = [
+    {url: "url1", //relative path for local images OR link to the image
+        altText: "altText1",
+    },
+    {url: "url2",
+        altText: "altText2",
+    },
+     {url: "url3",
+        altText: "altText3",
+    },
+];
+
+//TODO: I need to create thumbnail images
+// FOR EACH COMMENT REPLACE IT WITH ACTUAL CODE -->
+// 
+
+//TODO: I need to create my large images
