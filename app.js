4844b5a7047fa5d7f7edaaafaf517bb1e1e06e7b
Steph Kincarr <steph@polyhydral.co.uk>
2025-08-23 13:36:07 +0100

    Thumbnail display

--- app.js
+++ app.js
@@ -1,60 +1,45 @@
-// console.log("Kupo!");
-
 //TODO: I need to store my image data
-
 const images = [
-    {images: "./Images/Ahmed.jpg", //relative path for local images OR link to the image
-        altText: "A single sheet ghost wearing glasses in the forest",
-    },
-    {images: "./Images/EleanorBrooke.jpg",
-        altText: "A couple of sheet ghosts holding hands in the forest",
-    },
-     {images: "./Images/GiulioFabi1.jpg",
-        altText: "A couple of sheet ghosts with hats playing hide and seek amongst some trees",
-    },
-]
-// console.log(images) THESE RETURN! WOO! 
-
+  {
+    images: "./Images/Ahmed.jpg",
+    altText: "A single sheet ghost wearing glasses in the forest",
+  },
+  {
+    images: "./Images/EleanorBrooke.jpg",
+    altText: "A couple of sheet ghosts holding hands in the forest",
+  },
+  {
+    images: "./Images/GiulioFabi1.jpg",
+    altText:
+      "A couple of sheet ghosts with hats playing hide and seek amongst some trees",
+  },
+];
+// console.log(images) THESE RETURN! WOO!
 
 //TODO: I need to create thumbnail images
-// Function > createThumbnails
-const body = document.querySelector("body");
-const thumbnailContainer = document.querySelector(".thumbnailContainer");
+function createThumbnails() {
+  const thumbnailContainer = document.getElementById("thumbnail-container");
+  for (let i = 0; i < images.length; i++) {
+    const thumbnailImage = document.createElement("img");
+    thumbnailImage.setAttribute("src", images[i].images);
+    thumbnailImage.setAttribute("alt", images[i].altText);
+    thumbnailImage.className = "thumbnail-image";
 
-// Select the DOM element (thumbnail-container) to contain our thumbnails 
-onsole.log(images)
-    updateDisplayImage(images[currentImageIndex])
-    createThumbnails()
+    thumbnailImage.addEventListener("click", function () {
+      createLargeImageHandler(i);
+    });
 
-
-    //THIS IS A REPETITIVE TASK --> Loops through our array using the length property
-//loop inside
-    for (let image of images){
-
-// - create img element
-        const thumbnailImage = document.createElement('img')
-
-// - update src and alt attributes of the img element to match those in the array(PARAMETERS - Manny hint)
-        thumbnailImage.setAttribute('src', image.images)
-        thumbnailImage.setAttribute('alt', image.altText)
-
- // - give each img a className (need to make them small. img.className etc)
-    thumbnailImage.className = 'thumbnail-image'
-    let thumbnails = thumbnailImage('.thumbnailImage');
-
-    // - add an event listener to each image --> the event handle of this listener is the function you write to create large images (MANNY HINT)
-    //THINGGOES HERE
-        thumbnailImage.addEventListener("click", handleClick);
-       // - append the created images to the thumbnail-container
     thumbnailContainer.appendChild(thumbnailImage);
+  }
 }
 
-
-
-
+createThumbnails();
 
 //TODO: I need to create my large images
-// this task is the event handler for the thumbnail events 
+
+createLargeImageHandler(0);
+
+// this task is the event handler for the thumbnail events
 // function createLargeImagesHandler(){
 // select the large-image-container
 // delete the current image in the large-image-container (Actual code below)
@@ -66,3 +51,19 @@ onsole.log(images)
 // LOOPS ARE NOT. REPEAT. NOT. NEEDED FOR THIS ONE.
 //Add this event handler to the thumbnail event
 //make sure you call the createThumbnails function
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//THUMBNAIL TASK
+// Select the DOM element (thumbnail-container) to contain our thumbnails
+// updateDisplayImage(images[currentImageIndex])
+// createThumbnails()
+//THIS IS A REPETITIVE TASK --> Loops through our array using the length property
+//loop inside
+// - create img element
+// - update src and alt attributes of the img element to match those in the array(PARAMETERS - Manny hint)
+// - give each img a className (need to make them small. img.className etc)
+// let thumbnails = thumbnailImage('.thumbnailImage');
+// - add an event listener to each image --> the event handle of this listener is the function you write to create large images (MANNY HINT)
+// - append the created images to the thumbnail-container
+// thumbnailContainer.appendChild(thumbnailImage);
+//
